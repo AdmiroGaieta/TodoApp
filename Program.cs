f55df6a143ce5a27a67abfde863642afc46d19a5
Admiro Gaieta <mirogaieta100@gmail.com>
2024-04-06 22:03:17 +0100

    Adiciona aplicação automática de migrações

--- Program.cs
+++ Program.cs
@@ -1,23 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
-using  Microsoft.EntityFrameworkCore.Sqlite;
-using TodoAPP.Configuration;
-using TodoAPP.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using System.Text;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.Extensions.Options;
+using System.Text;
+using TodoAPP.Configuration;
+using TodoAPP.Data;
 using Microsoft.AspNetCore.Identity;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adiciona serviços ao contêiner...
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -26,38 +22,46 @@ builder.Services.Configure<JwtConfiguration>(builder.Configuration.GetSection("J
 builder.Services.AddDbContext<ApiDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-    builder.Services.AddAuthentication(options=>{
-        options.DefaultAuthenticateScheme=JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultScheme= JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme=JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(jwt=>{
-        var key=Encoding.ASCII.GetBytes(builder.Configuration["JwtConfiguration:Secret"]);
-        jwt.SaveToken=true;
-        jwt.TokenValidationParameters=new TokenValidationParameters{
-            ValidateIssuerSigningKey=true,
-            IssuerSigningKey= new SymmetricSecurityKey(key),
-            ValidateIssuer=false,
-            ValidateAudience=false,
-            ValidateLifetime=true,
-            RequireExpirationTime=true,
-        };
-    });
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-        .AddEntityFrameworkStores<ApiDbContext>();
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(jwt => {
+    var key = Encoding.ASCII.GetBytes(builder.Configuration["JwtConfiguration:Secret"]);
+    jwt.SaveToken = true;
+    jwt.TokenValidationParameters = new TokenValidationParameters {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        RequireExpirationTime = true,
+    };
+});
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApiDbContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
- 
+// Configura o pipeline de solicitação HTTP...
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
- 
+}
+
+// Aplica as migrações automaticamente
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApiDbContext>();
+    dbContext.Database.Migrate();
+}
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
